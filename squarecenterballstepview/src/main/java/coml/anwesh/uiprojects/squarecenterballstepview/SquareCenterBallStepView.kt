944bebd5823ba5f36237f2b70af152c8377bcf82
Anwesh43 <anweshthecool0@gmail.com>
2018-12-18 10:15:29 +0530

    adding libraries and defining constants

--- squarecenterballstepview/src/main/java/coml/anwesh/uiprojects/squarecenterballstepview/SquareCenterBallStepView.kt
+++ squarecenterballstepview/src/main/java/coml/anwesh/uiprojects/squarecenterballstepview/SquareCenterBallStepView.kt
@@ -3,3 +3,20 @@ package coml.anwesh.uiprojects.squarecenterballstepview
 /**
  * Created by anweshmishra on 18/12/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val balls : Int = 4
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.5f
+val color : Int = Color.parseColor("#283593")
