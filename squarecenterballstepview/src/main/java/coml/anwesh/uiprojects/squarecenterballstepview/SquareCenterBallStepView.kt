c85dfda6f0be015301b2a9d6ea58c98b235687a6
Anwesh43 <anweshthecool0@gmail.com>
2018-12-18 10:53:54 +0530

    adding SCBNode class

--- squarecenterballstepview/src/main/java/coml/anwesh/uiprojects/squarecenterballstepview/SquareCenterBallStepView.kt
+++ squarecenterballstepview/src/main/java/coml/anwesh/uiprojects/squarecenterballstepview/SquareCenterBallStepView.kt
@@ -120,4 +120,47 @@ class SquareCenterBallStepView(ctx : Context) : View(ctx) {
         }
     }
 
+    data class SCBNode(var i : Int, val state : State = State()) {
+
+        private var next : SCBNode? = null
+        private var prev : SCBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SCBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSCBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SCBNode {
+            var curr : SCBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
