e1a496591c11e974eb960cbf4a370879610b8651
Anwesh43 <anweshthecool0@gmail.com>
2018-12-18 10:58:18 +0530

    adding SquareBallCenterStep class

--- squarecenterballstepview/src/main/java/coml/anwesh/uiprojects/squarecenterballstepview/SquareCenterBallStepView.kt
+++ squarecenterballstepview/src/main/java/coml/anwesh/uiprojects/squarecenterballstepview/SquareCenterBallStepView.kt
@@ -160,7 +160,31 @@ class SquareCenterBallStepView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class SquareCenterBallStep(var i : Int) {
+
+        private val root : SCBNode = SCBNode(0)
+        private var curr : SCBNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
