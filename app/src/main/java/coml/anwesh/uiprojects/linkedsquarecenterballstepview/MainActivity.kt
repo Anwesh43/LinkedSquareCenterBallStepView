3c91fb1157ba395303e081c364c45dfb5327ea66
Anwesh43 <anweshthecool0@gmail.com>
2018-12-18 11:19:28 +0530

    adding fullscreen support

--- app/src/main/java/coml/anwesh/uiprojects/linkedsquarecenterballstepview/MainActivity.kt
+++ app/src/main/java/coml/anwesh/uiprojects/linkedsquarecenterballstepview/MainActivity.kt
@@ -2,11 +2,19 @@ package coml.anwesh.uiprojects.linkedsquarecenterballstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import coml.anwesh.uiprojects.squarecenterballstepview.SquareCenterBallStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquareCenterBallStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
