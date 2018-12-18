859e0153a2836c044214e3347bbb2675685675ea
Anwesh43 <anweshthecool0@gmail.com>
2018-12-18 10:08:37 +0530

    adding app module

---
+++ app/src/main/java/coml/anwesh/uiprojects/linkedsquarecenterballstepview/MainActivity.kt
@@ -0,0 +1,12 @@
+package coml.anwesh.uiprojects.linkedsquarecenterballstepview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
